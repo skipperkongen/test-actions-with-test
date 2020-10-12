b6a8df72128491fa1c84953bb1e9e2664774e934
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2020-10-12 18:51:06 +0200

    bananana

--- test/test_foo.py
+++ test/test_foo.py
@@ -6,10 +6,10 @@ class TestBasicFunction(unittest.TestCase):
         self.instance = Foo()
 
     def test_1(self):
-        self.assertEquals(self.instance.foo1(1), 2)
+        self.assertEqual(self.instance.foo1(1), 2)
 
     def test_2(self):
-        self.assertEquals(self.instance.foo2(1), 2)
+        self.assertEqual(self.instance.foo2(1), 2)
 
 if __name__ == '__main__':
     unittest.main()
